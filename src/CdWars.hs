e94b476d9934f85fc562a8115e6fa976f77e5fba
kjura <kjura@users.noreply.github.com>
2022-01-28 20:44:36 +0100

    Delete CdWars.hs

--- src/CdWars.hs
+++
@@ -1,9 +0,0 @@
-module CdWars where
-
-import Data.List
-
---orderedCart :: [Int] -> [(Int, Int)]
-orderedCart :: [Int] -> [(Int, Int)]
-orderedCart xs = [(x,y) | (x:rest) <- tails xs, y <- rest]
-
-
